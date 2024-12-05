5726c09663965a6858800f81c38064fc9adcee9e
Alexandre Rocha <alexandresobral2004@gmail.com>
2024-12-05 16:20:23 -0300

    update

--- Aula01/index.js
+++ Aula01/index.js
@@ -24,15 +24,28 @@ app.get('/contato', (req, res) => {
   res.render('contato')
 })
 
-//Recebe os dados de contato
+
+
+//Recebe os dados de
 app.post('/contato', (req, res) => {
   const name = req.body.name;
   const email = req.body.email;
   console.log(name, email)
 
-  res.render('contato')
+  // res.render('contato')
+  // res.send(`Obrigado ${name}, seu email ${email} foi enviado com sucesso`)
+
+  res.redirect(`/cadastro?name=${name}?email=${email}`)
 })
 
+app.get('/cadastro', (req, res) => {
+  const name = req.query.name;
+  const email = req.query.email;
+
+  console.log(name, email)
+
+  res.render('cadastro')
+})
 
 app.get('/', (req, res) => {
   res.render('home')
