d552e2960a65f940dc64a02f66abd746170c093a
André Bontempo <andre.lnx@gmail.com>
2025-01-06 22:05:34 -0300

    sdfg

--- 6_EXPRESS/2_setup/index.js
+++ 6_EXPRESS/2_setup/index.js
@@ -1,11 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Olá Mundoooo!!')
-})
+app.get("/", (req, res) => {
+  res.send("<h1>Olá Mundo!!</h1>");
+});
 
 app.listen(port, () => {
-  console.log(`App rodando na porta:${port}`)
-})
+  console.log(`App rodando na porta:${port}`);
+  console.log(`App rodando na porta:${port}`);
+});
