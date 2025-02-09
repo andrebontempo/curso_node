ae6dad641f11946ed6a6d5e3df699209d8a49b65
André Bontempo <andre.lnx@gmail.com>
2025-02-09 17:45:40 -0300

    ajustando

--- 6_EXPRESS/2_setup/index.js
+++ 6_EXPRESS/2_setup/index.js
@@ -1,12 +1,13 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+const express = require("express")
+const app = express()
+const port = 3000
 
 app.get("/", (req, res) => {
-  res.send("<h1>Olá Mundo!!</h1>");
-});
+  //res.send(`App rodando na porta:${port}`)
+  res.send("<h1>Teste</h1>")
+})
 
 app.listen(port, () => {
-  console.log(`App rodando na porta:${port}`);
-  console.log(`App rodando na porta:${port}`);
-});
+  console.log(`App rodando na porta:${port}`)
+  console.log(`App rodando na porta:${port}`)
+})
